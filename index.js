22cfcba1036200995c7f309fd5f839e94820ecb4
radioea <igrigrp@gmail.com>
2026-08-25 11:05:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ function search(query) {
   }
   // ★★★ ГЛАВНОЕ ИСПРАВЛЕНИЕ ★★★
   var sorted = Object.keys(results).sort(function(a, b) {
-    return (scores[b]  0) - (scores[a]  0);
+    return (scores[b] || 0) - (scores[a] || 0);
   });
   return sorted.map(function(key) {
     return { key: key, product: results[key], score: scores[key] || 0 };
