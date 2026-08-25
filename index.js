9e73bdf4fc1bef4ff318f8c5d3d0d91826839e0f
radioea <igrigrp@gmail.com>
2026-08-25 15:41:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ function search(query) {
   }
 
   var sorted = Object.keys(results).sort(function(a, b) {
-    return (scores[b]  0) - (scores[a]  0);
+    return (scores[b] || 0) - (scores[a] || 0);
   });
 
   return sorted.map(function(key) {
